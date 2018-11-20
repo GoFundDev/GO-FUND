c91f97a6345966eb152dab77922881e778d1c88a
GoFundDev <developer@go-fund-ico.com>
2018-11-20 22:26:16 +0000

    miner#2

--- src/miner.cpp
+++ src/miner.cpp
@@ -447,23 +447,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         pblock->nBits = GetNextWorkRequired(pindexPrev, pblock);
         pblock->nNonce = 0;
 
-        //Calculate the accumulator checkpoint only if the previous cached checkpoint need to be updated
-        uint256 nCheckpoint;
-        uint256 hashBlockLastAccumulated = chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash();
-        if (nHeight >= pCheckpointCache.first || pCheckpointCache.second.first != hashBlockLastAccumulated) {
-            //For the period before v2 activation, zPIV will be disabled and previous block's checkpoint is all that will be needed
-            pCheckpointCache.second.second = pindexPrev->nAccumulatorCheckpoint;
-            if (pindexPrev->nHeight + 1 >= Params().Zerocoin_Block_V2_Start()) {
-                AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
-                // the next time the accumulator checkpoint should be recalculated ( the next height that is multiple of 10)
-                pCheckpointCache.first = nHeight + (10 - (nHeight % 10));
-
-                // the block hash of the last block used in the accumulator checkpoint calc. This will handle reorg situations.
-                pCheckpointCache.second.first = hashBlockLastAccumulated;
-                pCheckpointCache.second.second = nCheckpoint;
-            }
-        }
-
+        uint256 nCheckpoint = 0;
         pblock->nAccumulatorCheckpoint = pCheckpointCache.second.second;
         pblocktemplate->vTxSigOps[0] = GetLegacySigOpCount(pblock->vtx[0]);
 
@@ -473,11 +457,6 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
             mempool.clear();
             return NULL;
         }
-
-//        if (pblock->IsZerocoinStake()) {
-//            CWalletTx wtx(pwalletMain, pblock->vtx[1]);
-//            pwalletMain->AddToWallet(wtx);
-//        }
     }
 
     return pblocktemplate.release();
